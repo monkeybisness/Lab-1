9bc66182c7774f9b15a4fbdcbd64fffe91c548eb
monkeybisness <User@DESKTOP-MIVQ3QL>
2023-10-12 08:44:45 +0500

    добавление библеотек

---
+++ Lab-1/Books.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    public class Book
+    {
+        public uint Id;
+
+        public string Author;
+
+        public string Title;
+
+        public uint PublicationYear;
+
+        public ushort CabinetNumber;
+
+        public byte ShelfNumber;
+
+    }
+}
--- Lab-1/Program.cs
+++ Lab-1/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using System.Reflection.PortableExecutable;
+
+namespace Lab
+{
+    class Program
+    {
+        CultureInfo cultureInfo = new CultureInfo("ru-RU", false);
+
+
+        public static void Main(string[] args)
+        { 
+            List<Book> books = new List<Book>
+            {
+                new Book
+                {
+                    Id = 1,
+
+                    Author = "Толстой Л. Н.",
+
+                    Title = "Война и Мир",
+
+                    PublicationYear = 1867,
+
+                    CabinetNumber = 1,
+
+                    ShelfNumber = 1
+                },
+
+                new Book
+                {
+                    Id = 2,
+
+                    Author = "Толстой Л. Н.",
+
+                    Title = "Анна Каренина",
+
+                    PublicationYear = 1878,
+
+                    CabinetNumber = 1,
+
+                    ShelfNumber = 1
+                },
+
+                new Book
+                {
+                    Id = 3,
+
+                    Author = "Толстой Л. Н.",
+
+                    Title = "Казаки",
+
+                    PublicationYear = 1863,
+
+                    CabinetNumber = 1,
+
+                    ShelfNumber = 2
+                },
+
+            };
+        }
+    }
+}
